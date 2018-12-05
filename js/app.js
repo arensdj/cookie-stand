48c8f5fef0f4afff2d78d49c3a7de1142af58ab7
Jasmin <jasmin.arensdorf@gmail.com>
2018-12-05 13:00:58 -0800

    Modified function that renders the totals on the footer of the table.  Needed to do a grand total of all the daily location totals.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
 
 // Global variables
 var storeHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
-var storeLocations = ['1st And Pike', 'SeaTac Airport', 'Seattle Center', 'Capital Hill', 'Alki'];
 
 // The sales table on html
 var salesTable = document.getElementById('sales-table');
 
 // Array that contains all of the Store objects 
+// Store.allStores = [];
 var allStores = [];
 
 // Definition of the Store object constructor
@@ -18,6 +18,7 @@ function Store(name, minCust, maxCust, avgCookieSale) {
   this.avgCookieSale = avgCookieSale;
   this.cookiesPurchasedPerHour = [];
   this.totalCookiesSold = 0;
+  // Store.allStores.push(this);
   this.generateRandNumCustomers = function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
   };
@@ -32,7 +33,6 @@ function Store(name, minCust, maxCust, avgCookieSale) {
       this.totalCookiesSold += this.cookiesPurchasedPerHour[i];
     }
   };
-  // Store.allStores.push(this);
 }
 
 Store.prototype.renderTable = function() {
@@ -62,19 +62,15 @@ Store.prototype.renderTable = function() {
 Store.renderHeader = function () {
   var headerRow = document.createElement('tr');
 
-  // Create a table header element
+  // Create a table header element, give it content and append to the header row.
   var thEl = document.createElement('th');
-  // Give it content.
   thEl.textContent = '                 ';
-  // Append to header row
   headerRow.appendChild(thEl);
 
   for(var i = 0; i < storeHours.length; i++) {
-    // Create a table header element
+    // Create a table header element, give it content and appeand to the header row
     thEl = document.createElement('th');
-    // Give it content.
     thEl.textContent = storeHours[i];
-    // Append to header row
     headerRow.appendChild(thEl);
   }
   thEl = document.createElement('th');
@@ -88,18 +84,18 @@ Store.renderHeader = function () {
 Store.generateHourlyTotals = function () {
   var totalCookiesPerHour = [];
   var trEl = document.createElement('tr');
-  // Create td element
+  // Create td element and add to the td element.
   var tdEl = document.createElement('td');
-  // Add content to td element
   tdEl.textContent = 'Totals';
   trEl.appendChild(tdEl);
   salesTable.append(trEl);
-
   var subtotal = 0;
   
+  // Calculate the total number of cookies per hour for each store and
+  // save this in an array
   for(var i = 0; i < storeHours.length; i++) {
     for(var j = 0; j < allStores.length; j++) {
-      subtotal = subtotal + allStores[j].cookiesPurchasedPerHour[i];
+      subtotal += allStores[j].cookiesPurchasedPerHour[i];
     }
     totalCookiesPerHour[i] = subtotal;
     subtotal = 0;
@@ -109,44 +105,46 @@ Store.generateHourlyTotals = function () {
     tdEl = document.createElement('td');
     tdEl.textContent = totalCookiesPerHour[i];
     trEl.appendChild(tdEl);
+    subtotal += totalCookiesPerHour[i];
   }
 
   // Adding another td so the table border is a solid box.
   tdEl = document.createElement('td');
-  tdEl.textContent = ' ';
+  // tdEl.textContent = ' ';
+  tdEl.textContent = subtotal;
   trEl.appendChild(tdEl);
 };
-
-Store.renderHeader();
 
 var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
 firstAndPike.calculateCookiesPerHour();
 firstAndPike.calculateTotalCookiesSold();
 allStores.push(firstAndPike);
-firstAndPike.renderTable();
 
 var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
 seaTacAirport.calculateCookiesPerHour();
 seaTacAirport.calculateTotalCookiesSold();
 allStores.push(seaTacAirport);
-seaTacAirport.renderTable();
 
 var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 seattleCenter.calculateCookiesPerHour();
 seattleCenter.calculateTotalCookiesSold();
 allStores.push(seattleCenter);
-seattleCenter.renderTable();
 
 var capitalHill = new Store('Capital Hill', 20, 38, 2.3);
 capitalHill.calculateCookiesPerHour();
 capitalHill.calculateTotalCookiesSold();
 allStores.push(capitalHill);
-capitalHill.renderTable();
 
 var alki = new Store('Alki', 2, 16, 4.6);
 alki.calculateCookiesPerHour();
 alki.calculateTotalCookiesSold();
 allStores.push(alki);
-alki.renderTable();
 
+// Render the table data
+Store.renderHeader();
+firstAndPike.renderTable();
+seaTacAirport.renderTable();
+seattleCenter.renderTable();
+capitalHill.renderTable();
+alki.renderTable();
 Store.generateHourlyTotals();
